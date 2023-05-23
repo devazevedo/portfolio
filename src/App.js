7952ee04bd9e4d1377af14aa0371da6fa2ec5bf4
azevedoafc <azevedoafc23@gmail.com>
2023-05-22 22:48:10 -0300

    portfolio quase terminado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills"
+import { Projects } from "./components/Projects"
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Skills/>
+      <Projects/>
+      <Footer/>
     </div>
   );
 }
